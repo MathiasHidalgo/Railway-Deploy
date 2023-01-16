999d83bbc2d1242bae1b7abff764cc128763ac66
MathiasHidalgo <mejhc2307lbc@gmail.com>
2023-01-16 18:11:07 -0500

    Second Commit

--- server/db.js
+++ server/db.js
@@ -1,1 +1,9 @@
- 
+import { createPool } from "mysql2";
+
+export const pool = createPool({
+  host: "localhost",
+  user: "root",
+  password: "passwords",
+  port: 3306,
+  database: "usersdb"
+})
