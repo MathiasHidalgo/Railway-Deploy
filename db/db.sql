cee10ff064ffab388b5c71de83663aa24e9a7ca8
MathiasHidalgo <mejhc2307lbc@gmail.com>
2023-01-16 21:06:32 -0500

    Just to warning

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,4 @@
+/*YOU NEED TO EXECUTE THIS CODE IN THE COMMAND PROMPT IN THE SV OF MYSQL*/
 /*Creating a table called users and it has and ID who increment everytime you acces to /create*/
 /*And a name that is written in VALUES*/
 CREATE TABLE users (
