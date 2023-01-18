9a10e9a63d58732864b2da924f14f855878f46e5
MathiasHidalgo <mejhc2307lbc@gmail.com>
2023-01-18 16:36:03 -0500

    Ready for production

--- server/db.js
+++ server/db.js
@@ -1,11 +1,19 @@
 //Importing the function createPool from mysql2 promises
 import { createPool } from "mysql2/promise";
 
+import {
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_PORT
+} from './config.js';
+
 //Exporting the pool or connection from js to mysql
 export const pool = createPool({
-  host: "localhost",
-  user: "root",
-  password: "Password2307",
-  port: 3306,
-  database: "usersdb" // you should create a database before adding it (In mySQL simple as, CREATE DATABASE usersdb;)
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+  database: DB_NAME // you should create a database before adding it (In mySQL simple as, CREATE DATABASE usersdb;)
 })
